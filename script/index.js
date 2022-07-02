a866824da78fc0038a0dab7fc4342dcffb25fd53
abhinildutt <abhinil.dutt@gmail.com>
2022-07-02 15:51:07 +0530

    scroll event

--- script/index.js
+++ script/index.js
@@ -1,20 +1,25 @@
-var n = document.getElementById("n");
-    n.onmouseover = function() {
-      var node = document.getElementById("node");
-      node.style.opacity = "0.5";
-  }
-
+// let items = documents.getElementsByClassName("node");
+// var ind = 0;
+// // document.addEventListener('scroll', () => {
+// //   if (document.body.scrollTop > 50) {
+// //     items[1].style.background = "blue";
+// //   }
+// // })
 const init = function() {
   let items = document.querySelectorAll("li");
   for (let i = 0; i < items.length; i++) {
     items[i].style.minHeight = "100vh";
-    if(i%2 == 0) items[i].style.background = "black";
-    else items[i].style.background = "skyblue";
+    items[i].style.background = "black";
   }  
   cssScrollSnapPolyfill();
+  const scrollEvent = () => {
+    if (window.scrollTop > 50) {
+        document.getElementById("node_here").style.opacity = "0";
+    } 
+  }
+  window.addEventListener('scroll', scrollEvent);
 };
 
 init();
-scrollbar();
 
 
