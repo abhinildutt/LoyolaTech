52e10a12b5ff407b24aadd9b2571699f8bb56527
abhinildutt <abhinil.dutt@gmail.com>
2022-07-04 01:33:15 +0530

    scroll

--- script/index.js
+++ script/index.js
@@ -1,25 +1,30 @@
-// let items = documents.getElementsByClassName("node");
-// var ind = 0;
-// // document.addEventListener('scroll', () => {
-// //   if (document.body.scrollTop > 50) {
-// //     items[1].style.background = "blue";
-// //   }
-// // })
+// var script = document.createElement('script');
+// script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+// document.getElementsByTagName('head')[0].appendChild(script);
+
 const init = function() {
   let items = document.querySelectorAll("li");
   for (let i = 0; i < items.length; i++) {
     items[i].style.minHeight = "100vh";
     items[i].style.background = "black";
   }  
-  cssScrollSnapPolyfill();
-  const scrollEvent = () => {
-    if (window.scrollTop > 50) {
-        document.getElementById("node_here").style.opacity = "0";
-    } 
+};
+console.log(document.getElementById("2page").offsetTop);
+var scrollbar = () => {
+
+  for(let i = 1; i <= 3; i++) {
+    $("#" + i + "node").click(function() {
+      $("#cont").css("scroll-snap-type", "none");
+      $(".container").animate({
+          scrollTop: document.getElementById(i + "page").offsetTop}, 'slow');
+    });
+      $("#cont").css("scroll-snap-type", "y mandatory");
+
   }
-  window.addEventListener('scroll', scrollEvent);
 };
 
-init();
 
+
+init();
+scrollbar();
 
